1ed24eeb12d48ae01712d04360e907426986e445
krall2125 <janrenski@gmail.com>
2024-11-17 10:11:19 +0100

    i ended up not doing it

--- .config/hypr/start.sh
+++ .config/hypr/start.sh
@@ -5,6 +5,7 @@ wl-paste --type text --watch cliphist store &
 wl-paste --type image --watch cliphist store &
 mako &
 swww-daemon &
-swww img ~/Wallpapers/gruvbox.png &
+swww img ~/Wallpapers/alexcirce.png &
 nm-applet --indicator &
 doas kanata -c ~/.config/kanata/config.kbd &
+/usr/lib/xdg-desktop-portal-hyprland &
